925d1aef07ca78a0e071668eb1d98a61b34bb3fb
Matt Superdock <msuperdock@gmail.com>
2023-09-14 14:08:52 -0500

    add project 2

---
+++ superdock/projects/project-2/payday.py
@@ -0,0 +1,8 @@
+# TODO: Write header comment here. (See "Style Guide" on Canvas. Delete this comment.)
+
+import math
+
+DAYS_PER_YEAR = 365
+MONTHS_PER_YEAR = 12
+
+# TODO: Write code here. (Delete this comment.)
